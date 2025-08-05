cf129f24a4bed1d647accb9cb31cbb73b159a6e1
AbhishekKaisar <abhishekkaisar2015@gmail.com>
2025-08-05 22:19:33 +0600

    Chapter 2: Parser implemented

--- toy.cpp
+++ toy.cpp
@@ -19,6 +19,74 @@ enum Token {
 static std::string IdentifierStr;
 static double NumVal;
 
+// Base class for all expression nodes.
+class ExprAST {
+public:
+  virtual ~ExprAST() = default;
+};
+
+// Expression class for numeric literals like "1.0".
+class NumberExprAST : public ExprAST {
+  double Val;
+
+public:
+  NumberExprAST(double Val) : Val(Val) {}
+};
+
+// Expression class for referencing a variable, like "a".
+
+class VariableExprAST : public ExprAST {
+  std::string Name;
+
+public:
+  VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+// Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+  char Op;
+  std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+  BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+                std::unique_ptr<ExprAST> RHS)
+      : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+// Expression class for function calls.
+class CallExprAST : public ExprAST {
+  std::string Callee;
+  std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+  CallExprAST(const std::string &Callee,
+              std::vector<std::unique_ptr<ExprAST>> Args)
+      : Callee(Callee), Args(std::move(Args)) {}
+};
+
+// This class represents the "prototype" for a function.
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+
+public:
+  PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+      : Name(Name), Args(std::move(Args)) {}
+
+  const std::string &getName() const { return Name; }
+};
+
+// This class represents a function definition.
+class FunctionAST {
+  std::unique_ptr<PrototypeAST> Proto;
+  std::unique_ptr<ExprAST> Body;
+
+public:
+  FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+              std::unique_ptr<ExprAST> Body)
+      : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
 int gettok() {
   static int LastChar = ' ';
 
@@ -61,11 +129,86 @@ int gettok() {
   return ThisChar;
 }
 
+// === PARSER BASICS ===
+static int CurTok;
+int getNextToken() { return CurTok = gettok(); }
+
+std::unique_ptr<ExprAST> LogError(const char *Str) {
+  std::cerr << "Error: " << Str << "\n";
+  return nullptr;
+}
+
+std::unique_ptr<ExprAST> ParseNumberExpr() {
+  auto Result = std::make_unique<NumberExprAST>(NumVal);
+  getNextToken(); // consume the number
+  return std::move(Result);
+}
+
+std::unique_ptr<ExprAST> ParseParenExpr() {
+  getNextToken(); // eat '('
+  auto V = ParseExpression();
+  if (!V) return nullptr;
+
+  if (CurTok != ')')
+    return LogError("expected ')'");
+  getNextToken(); // eat ')'
+  return V;
+}
+
+std::unique_ptr<ExprAST> ParseIdentifierExpr() {
+  std::string IdName = IdentifierStr;
+
+  getNextToken(); // eat identifier
+
+  if (CurTok != '(')
+    return std::make_unique<VariableExprAST>(IdName);
+
+  // Function call
+  getNextToken(); // eat '('
+  std::vector<std::unique_ptr<ExprAST>> Args;
+  if (CurTok != ')') {
+    while (true) {
+      if (auto Arg = ParseExpression())
+        Args.push_back(std::move(Arg));
+      else
+        return nullptr;
+
+      if (CurTok == ')') break;
+
+      if (CurTok != ',') return LogError("Expected ')' or ',' in argument list");
+      getNextToken();
+    }
+  }
+
+  getNextToken(); // eat ')'
+  return std::make_unique<CallExprAST>(IdName, std::move(Args));
+}
+
+std::unique_ptr<ExprAST> ParsePrimary() {
+  switch (CurTok) {
+    case tok_identifier: return ParseIdentifierExpr();
+    case tok_number:     return ParseNumberExpr();
+    case '(':            return ParseParenExpr();
+    default:             return LogError("unknown token when expecting an expression");
+  }
+}
+
+std::unique_ptr<ExprAST> ParseExpression() {
+  return ParsePrimary();
+}
+
 int main() {
+  std::cout << "Ready> ";
+  getNextToken();
+
   while (true) {
-    int tok = gettok();
-    std::cout << "Token: " << tok << "\n";
-    if (tok == tok_eof) break;
+    std::cout << "Ready> ";
+    if (CurTok == tok_eof) break;
+    if (CurTok == ';') {
+      getNextToken();
+      continue;
+    }
+    ParseExpression();
   }
   return 0;
 }

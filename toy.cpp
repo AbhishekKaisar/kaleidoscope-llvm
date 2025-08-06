d4d6b866aa6c9d59f5111ca5b16ce5095730ed6f
AbhishekKaisar <abhishekkaisar2015@gmail.com>
2025-08-06 12:02:26 +0600

    Chapter 3: Fixed REPL and added error handling

--- toy.cpp
+++ toy.cpp
@@ -3,6 +3,12 @@
 #include <string>
 #include <cctype>
 #include <iostream>
+#include <map>
+
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
 
 enum Token {
   tok_eof = -1,
@@ -19,10 +25,44 @@ enum Token {
 static std::string IdentifierStr;
 static double NumVal;
 
+static std::map<std::string, llvm::Value *> NamedValues;
+
+
+static std::unique_ptr<llvm::LLVMContext> TheContext;
+static std::unique_ptr<llvm::IRBuilder<>> Builder;
+static std::unique_ptr<llvm::Module> TheModule;
+
+
+
+// Function template for error logging, with specializations.
+template<typename T>
+T LogError(const char *Str);
+
+// Dummy derived class to allow returning a unique_ptr<ExprAST>
+class DummyExprAST : public ExprAST {
+public:
+  llvm::Value *codegen() override {
+    return nullptr;
+  }
+};
+
+template<>
+llvm::Value *LogError<llvm::Value *>(const char *Str) {
+  std::cerr << "Error: " << Str << "\n";
+  return nullptr;
+}
+
+template<>
+std::unique_ptr<ExprAST> LogError<std::unique_ptr<ExprAST>>(const char *Str) {
+  std::cerr << "Error: " << Str << "\n";
+  return std::make_unique<DummyExprAST>();
+}
+
 // Base class for all expression nodes.
 class ExprAST {
 public:
   virtual ~ExprAST() = default;
+  virtual llvm::Value *codegen() = 0;
 };
 
 // Expression class for numeric literals like "1.0".
@@ -31,6 +71,7 @@ class NumberExprAST : public ExprAST {
 
 public:
   NumberExprAST(double Val) : Val(Val) {}
+  llvm::Value *codegen();
 };
 
 // Expression class for referencing a variable, like "a".
@@ -40,6 +81,7 @@ class VariableExprAST : public ExprAST {
 
 public:
   VariableExprAST(const std::string &Name) : Name(Name) {}
+  llvm::Value *codegen();
 };
 
 // Expression class for a binary operator.
@@ -51,6 +93,26 @@ public:
   BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
                 std::unique_ptr<ExprAST> RHS)
       : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+  llvm::Value *codegen() override {
+    llvm::Value *L = LHS.get()->codegen();
+    llvm::Value *R = RHS.get()->codegen();
+    if (!L || !R)
+      return nullptr;
+
+    switch (Op) {
+      case '+':
+        return Builder->CreateFAdd(L, R, "addtmp");
+      case '-':
+        return Builder->CreateFSub(L, R, "subtmp");
+      case '*':
+        return Builder->CreateFMul(L, R, "multmp");
+      case '<':
+        L = Builder->CreateFCmpULT(L, R, "cmptmp");
+        return Builder->CreateUIToFP(L, llvm::Type::getDoubleTy(*TheContext), "booltmp");
+      default:
+        return LogError<llvm::Value*>("invalid binary operator");
+    }
+  }
 };
 
 // Expression class for function calls.
@@ -62,6 +124,24 @@ public:
   CallExprAST(const std::string &Callee,
               std::vector<std::unique_ptr<ExprAST>> Args)
       : Callee(Callee), Args(std::move(Args)) {}
+  llvm::Value *codegen() override {
+    llvm::Function *CalleeF = TheModule->getFunction(Callee);
+    if (!CalleeF)
+      return LogError<llvm::Value*>("Unknown function referenced");
+
+    if (CalleeF->arg_size() != Args.size())
+      return LogError<llvm::Value*>("Incorrect # arguments passed");
+
+    std::vector<llvm::Value *> ArgsV;
+    for (unsigned i = 0, e = Args.size(); i != e; ++i) {
+      llvm::Value *ArgVal = Args[i].get()->codegen();
+      if (!ArgVal)
+        return nullptr;
+      ArgsV.push_back(ArgVal);
+    }
+
+    return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
+  }
 };
 
 // This class represents the "prototype" for a function.
@@ -133,10 +213,6 @@ int gettok() {
 static int CurTok;
 int getNextToken() { return CurTok = gettok(); }
 
-std::unique_ptr<ExprAST> LogError(const char *Str) {
-  std::cerr << "Error: " << Str << "\n";
-  return nullptr;
-}
 
 // Forward declaration for ParseExpression, used in ParseParenExpr and elsewhere.
 std::unique_ptr<ExprAST> ParseExpression();
@@ -153,7 +229,7 @@ std::unique_ptr<ExprAST> ParseParenExpr() {
   if (!V) return nullptr;
 
   if (CurTok != ')')
-    return LogError("expected ')'");
+    return LogError<std::unique_ptr<ExprAST>>("expected ')'");
   getNextToken(); // eat ')'
   return V;
 }
@@ -178,7 +254,7 @@ std::unique_ptr<ExprAST> ParseIdentifierExpr() {
 
       if (CurTok == ')') break;
 
-      if (CurTok != ',') return LogError("Expected ')' or ',' in argument list");
+      if (CurTok != ',') return LogError<std::unique_ptr<ExprAST>>("Expected ')' or ',' in argument list");
       getNextToken();
     }
   }
@@ -192,7 +268,7 @@ std::unique_ptr<ExprAST> ParsePrimary() {
     case tok_identifier: return ParseIdentifierExpr();
     case tok_number:     return ParseNumberExpr();
     case '(':            return ParseParenExpr();
-    default:             return LogError("unknown token when expecting an expression");
+    default:             return LogError<std::unique_ptr<ExprAST>>("unknown token when expecting an expression");
   }
 }
 
@@ -200,7 +276,22 @@ std::unique_ptr<ExprAST> ParseExpression() {
   return ParsePrimary();
 }
 
+llvm::Value *NumberExprAST::codegen() {
+  return llvm::ConstantFP::get(*TheContext, llvm::APFloat(Val));
+}
+
+llvm::Value *VariableExprAST::codegen() {
+  llvm::Value *V = NamedValues[Name];
+  if (!V)
+    return LogError<llvm::Value*>("Unknown variable name");
+  return V;
+}
+
 int main() {
+  TheContext = std::make_unique<llvm::LLVMContext>();
+  TheModule = std::make_unique<llvm::Module>("my cool jit", *TheContext);
+  Builder = std::make_unique<llvm::IRBuilder<>>(*TheContext);
+
   std::cout << "Ready> ";
   getNextToken();
 
@@ -211,7 +302,10 @@ int main() {
       getNextToken();
       continue;
     }
-    ParseExpression();
+    if (auto IR = ParseExpression()->codegen()) {
+      IR->print(llvm::errs());
+      std::cerr << "\n";
+    }
   }
   return 0;
 }
